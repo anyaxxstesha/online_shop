66519a184700e0b54c9afc4d872256f08bed65d9
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-03-03 23:43:52 +0300

    add category class

---
+++ src/category_class.py
@@ -0,0 +1,20 @@
+class Category:
+    """Класс для представления категории."""
+
+    name: str
+    description: str
+    products: list
+
+    # Переменная на уровне класса для подсчета количества категорий
+    number_of_categories = 0
+    # Переменная на уровне класса для подсчета количества уникальных товаров(длина множества)
+    number_of_products = set()
+
+    def __init__(self, name, description, products):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.number_of_categories += 1
+        Category.number_of_products = Category.number_of_goods.union(set(products))
