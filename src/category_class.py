68e26d821836b60f354df068f3f623d2794180c2
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-03-04 22:57:37 +0300

    fix bug

--- src/category_class.py
+++ src/category_class.py
@@ -7,8 +7,8 @@ class Category:
 
     # Переменная на уровне класса для подсчета количества категорий
     number_of_categories = 0
-    # Переменная на уровне класса для подсчета количества уникальных товаров(длина множества)
-    number_of_products = set()
+    # Переменная на уровне класса для подсчета количества уникальных товаров
+    number_of_products = 0
 
     def __init__(self, name, description, products):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
@@ -17,4 +17,4 @@ class Category:
         self.products = products
 
         Category.number_of_categories += 1
-        Category.number_of_products = Category.number_of_goods.union(set(products))
+        Category.number_of_products += len(self.products)
