1cc798296b2f6a1e8237586935143bb79a76c42e
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-03-08 22:51:55 +0300

    make category.products a private attribute and create method add_product

--- src/category_class.py
+++ src/category_class.py
@@ -3,7 +3,7 @@ class Category:
 
     name: str
     description: str
-    products: list
+    __products: list
 
     # Переменная на уровне класса для подсчета количества категорий
     number_of_categories = 0
@@ -14,7 +14,13 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.number_of_categories += 1
-        Category.number_of_products += len(self.products)
+        Category.number_of_products += len(self.__products)
+
+    def add_product(self, product):
+        """
+        Принимает объект товара и добавляет в список товаров категории
+        """
+        self.__products.append(product)
