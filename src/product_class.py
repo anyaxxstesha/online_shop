55b9a540b875601d8b300b28a2d557ec8f1ad5df
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-03-08 23:39:35 +0300

    add create_product class-method with additional control of products copies

--- src/product_class.py
+++ src/product_class.py
@@ -6,9 +6,28 @@ class Product:
     price: float
     quantity: int
 
+    goods_created = []
+
     def __init__(self, name, description, price, quantity):
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def create_product(cls, name, description, price, quantity):
+        """
+        Создаёт товар и возвращает объект, дополнительно проверяя наличие такого же товара
+        """
+        self = cls(name, description, price, quantity)
+
+        for good in cls.goods_created:
+            if self.name == good.name:
+                self.quantity += good.quantity
+                self.price = max(self.price, good.price)
+                cls.goods_created.remove(good)
+                break
+
+        cls.goods_created.append(self)
+        return self
