72b486a070bfca39f78090d094bea962959d08c8
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-03-08 23:55:14 +0300

    add getter and setter for price attribute(product class)

--- src/product_class.py
+++ src/product_class.py
@@ -3,7 +3,7 @@ class Product:
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     goods_created = []
@@ -12,7 +12,7 @@ class Product:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
@@ -25,9 +25,27 @@ class Product:
         for good in cls.goods_created:
             if self.name == good.name:
                 self.quantity += good.quantity
-                self.price = max(self.price, good.price)
+                self.__price = max(self.__price, good.price)
                 cls.goods_created.remove(good)
                 break
 
         cls.goods_created.append(self)
         return self
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """
+        Устанавливает новую цену для товара, при понижении цены спрашивает подтверждение
+        """
+        if new_price <= 0:
+            print('Введена некорректная цена')
+        elif new_price <= self.__price:
+            confirmation = input('Вы уверены, что хотите понизить цену? y - да, n - нет')
+            if confirmation == 'y':
+                self.__price = new_price
+        else:
+            self.__price = new_price
