23707071543944b03c3b574b5ef35a8086f98872
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-07-17 23:50:46 +0300

    configure forms and formset for version creation

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,19 @@
-from django.forms import ModelForm, forms
+from django.forms import ModelForm, forms, BooleanField
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = ("name", "description", "image", "category", "price")
@@ -27,3 +37,9 @@ class ProductForm(ModelForm):
         self.check_restrictions(description, 'description')
 
         return description
+
+
+class VersionForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Version
+        fields = ("product", "version_number", "version_name", "is_current_version")
