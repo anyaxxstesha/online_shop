522352489ada67c5e901d2f3c06aad2dbf01a295
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-07-18 17:33:28 +0300

    start users app and create user model

---
+++ users/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name='email address'
+    )
+
+    phone = models.CharField(
+        max_length=35,
+        verbose_name='Номер телефона',
+        blank=True, null=True,
+        help_text='Введите номер телефона в формате +7 (XXX)')
+    country = models.CharField(
+        max_length=100,
+        verbose_name='Страна',
+        blank=True, null=True,
+        help_text='Выберите страну'
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        verbose_name='Аватар',
+        blank=True, null=True,
+        help_text='Загрузите аватар'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.email
