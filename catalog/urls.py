2fefeea7f59ccf3406d7f8f76f68bb0abb75e448
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-07-20 21:57:35 +0300

    add caching for controller responsible for viewing a product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, ContactsView, ProductCreateView, ProductUpdateView, \
     ProductDeleteView
@@ -8,7 +10,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_info'),
+    path('catalog/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_info'),
     path('catalog/create/', ProductCreateView.as_view(), name='product_create'),
     path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
